862a999f9e8f2587af1fcf350446685ae601ff27
lyq <524123601@qq.com>
2023-10-30 23:37:01 +0800

    Rspack 0.3.8 compiled with 67 errors and 8 warnings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { ProxyAgent, setGlobalDispatcher } from 'undici';
 import { fetchSourceExtension } from './fetch-source.ts';
+
+setGlobalDispatcher(new ProxyAgent('127.0.0.1:7890'));
 
 (async () => {
     try {
